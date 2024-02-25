fcec7fc17e339718e3cfcca57a1dbe9506fce02b
vanshika704 <sharmavanshi704@gmail.com>
2024-02-25 14:30:57 +0530

    contact list

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:whatsapp_messenger/Signup.dart';
 import 'package:whatsapp_messenger/firebase_options.dart';
 import 'package:whatsapp_messenger/page1.dart';
 
  Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Permission.contacts.request();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
--- lib/page1.dart
+++ lib/page1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:contacts_service/contacts_service.dart';
 import 'package:whatsapp_messenger/chats.dart';
 
 class Page1 extends StatelessWidget {
@@ -8,7 +9,7 @@ class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 4, 
+      length: 4,
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Color.fromARGB(255, 54, 54, 54),
@@ -22,14 +23,15 @@ class Page1 extends StatelessWidget {
           ),
           actions: [
             IconButton(
-              icon: Icon(Icons.search,color: Colors.white,),
+              icon: Icon(Icons.search, color: Colors.white),
               onPressed: () {},
             ),
             IconButton(
-              icon: Icon(Icons.camera_alt,color: Colors.white,),
+              icon: Icon(Icons.camera_alt, color: Colors.white),
               onPressed: () {},
             ),
-            PopupMenuButton(color: Colors.white,
+            PopupMenuButton(
+              color: Colors.white,
               itemBuilder: (BuildContext context) {
                 return [
                   PopupMenuItem(
@@ -55,22 +57,21 @@ class Page1 extends StatelessWidget {
             ),
           ],
           bottom: const TabBar(
-  labelColor: Colors.white, 
-  unselectedLabelColor: Colors.white, 
-  tabs: [
-    Tab(icon: Icon(Icons.group)),
-    Tab(
-      text: "CHATS",
-    ),
-    Tab(
-      text: "STATUS",
-    ),
-    Tab(
-      text: "CALLS",
-    ),
-  ],
-),
-
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white,
+            tabs: [
+              Tab(icon: Icon(Icons.group)),
+              Tab(
+                text: "CHATS",
+              ),
+              Tab(
+                text: "STATUS",
+              ),
+              Tab(
+                text: "CALLS",
+              ),
+            ],
+          ),
         ),
         body: Chats(),
       ),
@@ -78,46 +79,45 @@ class Page1 extends StatelessWidget {
   }
 }
 
-class ChatController extends GetxController {
-  RxList<Map<String, String>> chatData = [
-    {"name": "John Doe", "message": "Hello!", "imageUrl": "url_to_image"},
-    {"name": "Jane Doe", "message": "Hi there!", "imageUrl": "url_to_image"},
-  ].obs;
+class Chats extends StatefulWidget {
+  @override
+  _ChatsState createState() => _ChatsState();
 }
 
-class Chats extends StatelessWidget {
-  final ChatController chatController = Get.put(ChatController());
+class _ChatsState extends State<Chats> {
+  List<Contact> _contacts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadContacts();
+  }
+
+  Future<void> _loadContacts() async {
+    Iterable<Contact> contacts = await ContactsService.getContacts();
+    setState(() {
+      _contacts = contacts.toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Obx(
-      () => ListView.builder(
-        itemCount: chatController.chatData.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            key: UniqueKey(), 
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(
-                  chatController.chatData[index]['imageUrl'] ?? ''),
-            ),
-            title: Text(
-              chatController.chatData[index]['name'] ?? '',
-              style: TextStyle(color: Color.fromARGB(255, 253, 253, 253)),
-            ),
-            subtitle: Text(
-              chatController.chatData[index]['message'] ?? '',
-              style: TextStyle(color: Color.fromARGB(255, 253, 253, 253)),
-            ),
-            tileColor: Color.fromARGB(255, 37, 37, 37),
-            onTap: () {
-              Get.to(PersonalChat(), arguments: index);
-            },
-          );
-        },
-      ),
+    return ListView.builder(
+      itemCount: _contacts.length,
+      itemBuilder: (BuildContext context, int index) {
+        Contact contact = _contacts[index];
+        return ListTile(
+          leading: CircleAvatar(
+            backgroundImage: contact.avatar != null
+                ? MemoryImage(contact.avatar!)
+                : AssetImage('assets/default.png') as ImageProvider<Object>?,
+          ),
+          title: Text(contact.displayName ?? ''),
+          onTap: () {
+            Get.to(PersonalChat(), arguments: _contacts[index]);
+          },
+        );
+      },
     );
   }
 }
-
-
-
