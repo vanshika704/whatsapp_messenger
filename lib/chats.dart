739c237f308045ac7bc29e586521f0faf4f77b95
vanshika704 <sharmavanshi704@gmail.com>
2024-03-02 15:51:55 +0530

    commit

--- lib/chats.dart
+++ lib/chats.dart
@@ -1,15 +1,11 @@
 import 'package:contacts_service/contacts_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/get_navigation.dart';
-import 'package:jitsi_meet/jitsi_meet.dart';
-import 'dart:math';
+import 'package:flutter/widgets.dart';
+
 import 'package:whatsapp_messenger/common/colors.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
-
 
 class PersonalChat extends StatefulWidget {
   final Contact contact;
@@ -23,6 +19,8 @@ class PersonalChat extends StatefulWidget {
 class _PersonalChatState extends State<PersonalChat> {
   final TextEditingController _messageController = TextEditingController();
   FirebaseMessaging messaging = FirebaseMessaging.instance;
+  List<String> _messages = [];
+
   @override
   void initState() {
     super.initState();
@@ -55,16 +53,14 @@ class _PersonalChatState extends State<PersonalChat> {
             ),
             const SizedBox(width: 8),
             IconButton(
-                onPressed: () async {
-                await Get.to(_joinMeeting());
-                },
-
-                icon: const Icon(
-                  Icons.video_call_outlined,
-                  color: Colors.white,
-                )),
+              onPressed: () async {},
+              icon: const Icon(
+                Icons.video_call_outlined,
+                color: Colors.white,
+              ),
+            ),
             PopupMenuButton(
-              color: Colors.white,
+              icon: Icon(Icons.more, color: Colors.white),
               itemBuilder: (BuildContext context) {
                 return [
                   const PopupMenuItem(
@@ -98,28 +94,25 @@ class _PersonalChatState extends State<PersonalChat> {
                   child: TextField(
                     controller: _messageController,
                     keyboardType: TextInputType.emailAddress,
-                    style:
-                        const TextStyle(color: Color.fromARGB(255, 14, 12, 12)),
+                    style: const TextStyle(color: Color.fromARGB(255, 14, 12, 12)),
                     decoration: const InputDecoration(
                       labelText: 'Type.....',
-                      labelStyle:
-                          TextStyle(color: Color.fromARGB(255, 14, 13, 13)),
+                      labelStyle: TextStyle(color: Color.fromARGB(255, 14, 13, 13)),
                       hintText: 'Type text....',
-                      hintStyle:
-                          TextStyle(color: Color.fromARGB(255, 10, 10, 10)),
+                      hintStyle: TextStyle(color: Color.fromARGB(255, 10, 10, 10)),
                       prefixIcon: Icon(
                         Icons.camera,
                         color: Color.fromARGB(255, 8, 8, 8),
                       ),
-                      filled: false,
+                      filled: true,
+                      fillColor: Colors.transparent,
                     ),
                   ),
                 ),
                 const SizedBox(width: 2),
                 InkWell(
                   onTap: _sendMessage,
-                  child: const Icon(Icons.send,
-                      color: Color.fromARGB(255, 12, 12, 12)),
+                  child: const Icon(Icons.send, color: Color.fromARGB(255, 7, 7, 7)),
                 ),
               ],
             ),
@@ -129,49 +122,64 @@ class _PersonalChatState extends State<PersonalChat> {
     );
   }
 
-  Widget _buildChatHistory() {
-    return StreamBuilder<QuerySnapshot>(
-      stream: FirebaseFirestore.instance
-          .collection('your_collection')
-          .where('senderId', isEqualTo: FirebaseAuth.instance.currentUser?.uid)
-          .where('receiverId', isEqualTo: widget.contact.identifier)
-          .orderBy('timestamp')
-          .snapshots(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        }
-
-        if (snapshot.hasError) {
-          return Center(
-            child: Text('Error: ${snapshot.error}'),
-          );
-        }
-
-        var messages = snapshot.data?.docs ?? [];
-
-        return ListView.builder(
-          itemCount: messages.length,
-          itemBuilder: (context, index) {
-            var messageData = messages[index].data() as Map<String, dynamic>;
-            return ListTile(
-              minLeadingWidth: 10,
-              title: Text(
-                messageData['message'],
-                style: const TextStyle(color: Colors.white),
-              ),
-            );
-          },
-        );
-      },
-    );
-  }
+ Widget _buildChatHistory() {
+  return Column(
+    children: [
+      Expanded(
+        child: Container(
+          color: Coloors.backgroundDark(),
+          child: ListView.builder(
+            itemCount: _messages.length,
+            itemBuilder: (context, index) {
+              return Dismissible(
+                key: Key(_messages[index]),
+                onDismissed: (direction) {
+                  setState(() {
+                    _messages.removeAt(index);
+                  });
+                },
+                background: Container(
+                  color: Colors.red, // You can customize the background color
+                  alignment: Alignment.centerRight,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: const Icon(
+                    Icons.delete,
+                    color: Colors.white,
+                  ),
+                ),
+                child: ListTile(
+                  minLeadingWidth: 2,
+                  tileColor: Coloors.backgroundDark(),
+                  title: GestureDetector(
+                    onHorizontalDragEnd: (details) {
+                      if (details.primaryVelocity! < 0) {
+                        // User swiped left (negative velocity), you can perform some action here
+                      } else if (details.primaryVelocity! > 0) {
+                        // User swiped right (positive velocity), you can perform some action here
+                      }
+                    },
+                    child: Text(
+                      _messages[index],
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ),
+    ],
+  );
+}
 
   void _sendMessage() {
     String message = _messageController.text.trim();
     if (message.isNotEmpty) {
+      setState(() {
+        _messages.add(message);
+      });
+
       FirebaseFirestore.instance.collection('your_collection').add({
         'senderId': FirebaseAuth.instance.currentUser?.uid,
         'receiverId': widget.contact.identifier,
@@ -183,23 +191,3 @@ class _PersonalChatState extends State<PersonalChat> {
     }
   }
 }
-
-void _handlePopupMenuSelection(String value) {
-  if (value == "block") {
-  } else if (value == "wallpaper") {
-  } else if (value == "clear_chat") {}
-}
-String generateRandomRoomName() {
-  final random = Random();
-  return 'room_${random.nextInt(9999)}';
-}
- _joinMeeting() async {
-    try {
-      var roomName = generateRandomRoomName();
-      var options = JitsiMeetingOptions(room: roomName);
-      await JitsiMeet.joinMeeting(options);
-    } catch (error) {
-      print("Error: $error");
-    }
-  }
- 
