edf8f6d31503d1c84da31356b0d80ee1c345dd3a
vanshika704 <sharmavanshi704@gmail.com>
2024-03-26 19:52:37 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,19 +17,19 @@ import 'package:onesignal_flutter/onesignal_flutter.dart';
   FirebaseFirestore.instance.settings = const Settings(
     persistenceEnabled: true,
   );
-//Remove this method to stop OneSignal Debugging 
+
 OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
 
 OneSignal.initialize("c41ac4b7-944e-42b4-9071-4abe12bf2fca");
 
-// The promptForPushNotificationsWithUserResponse function will show the iOS or Android push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+
 OneSignal.Notifications.requestPermission(true);
   runApp(const MyApp());
   
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key});//.
 
   @override
   Widget build(BuildContext context) {
